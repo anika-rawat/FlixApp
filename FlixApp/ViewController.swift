63f229560bcbfe34b24d469e94db9416ec8d6bbb
Anika <anikarawat@berkeley.edu>
2020-09-25 23:02:54 -0500

    Initial Commit

---
+++ FlixApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlixApp
+//
+//  Created by Anika Rawat on 9/25/20.
+//  Copyright © 2020 arawat00@icloud.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
